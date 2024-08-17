f5b16f20dd3d592d35ece7193a5e791f28487fb1
Yasmine-Hmida <hmidayasmiine@gmail.com>
2024-08-17 15:27:38 +0100

    Modal Version2

--- script2.js
+++ script2.js
@@ -1,7 +1,10 @@
-// this is the buy button 
+// Manipulating the Modal
 const productButton = document.querySelector(".productButton"); 
+const payButton = document.querySelector(".payButton");
 const payment = document.querySelector(".payment");
 const close = document.querySelector(".close");
+const cardIcons = document.querySelectorAll(".cardIcon");
+const orderDone = document.querySelector(".done");
 
 productButton.addEventListener("click",()=>{
     payment.style.display="flex";
@@ -10,3 +13,14 @@ productButton.addEventListener("click",()=>{
 close.addEventListener("click",()=>{
     payment.style.display="none";
 })
+
+cardIcons.forEach((cardIcon) => {
+    cardIcon.addEventListener("click", (event) => {
+        event.target.classList.toggle("selected");
+    });
+    
+});
+
+payButton.addEventListener("click",()=>{
+    orderDone.innerHTML="Ordered! Thanks For Shopping from Us!";
+});
