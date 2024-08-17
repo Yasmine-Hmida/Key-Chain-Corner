e1f69454fda88b76580ebbe42d60b619025289cf
Yasmine-Hmida <hmidayasmiine@gmail.com>
2024-08-17 13:06:17 +0100

    Modal Creation Version1

---
+++ script2.js
@@ -0,0 +1,12 @@
+// this is the buy button 
+const productButton = document.querySelector(".productButton"); 
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click",()=>{
+    payment.style.display="flex";
+});
+
+close.addEventListener("click",()=>{
+    payment.style.display="none";
+})
