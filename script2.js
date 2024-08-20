4e3f46beb0224511af17302e092074665f74ebe2
Yasmine-Hmida <hmidayasmiine@gmail.com>
2024-08-20 18:51:43 +0100

    Add Form Validation!

--- script2.js
+++ script2.js
@@ -27,7 +27,56 @@ cardIcons.forEach((cardIcon) => {
     
 });
 
-// Hnadle the Submitting
+let visaClicked = false;
+let masterCardClicked = false;
+// Hnadle the Submitting and Form Validation
 payButton.addEventListener("click",()=>{
-    orderDone.innerHTML="Ordered! Thanks For Shopping from Us!";
+    // Get the Input Fields Every time you Click
+    const fullName = document.querySelector("#name").value;
+    const phoneNumber = document.querySelector("#phoneNumber").value;
+    const Address = document.querySelector("#Address").value;
+    const visa = document.querySelector("#visa");
+    const masterCard = document.querySelector("#mastercard");
+    const cardNumber = document.querySelector("#cardNumber").value;
+    const month = document.querySelector("#month").value;
+    const day = document.querySelector("#day").value;
+    const year = document.querySelector("#year").value;
+
+    // Add Event Listeners for the visa and MasterCard
+    visa.addEventListener('click', function() {
+        visaClicked = true;
+    });
+    
+    masterCard.addEventListener('click', function() {
+        masterCardClicked = true;
+    });
+    
+    // Form Validation
+    if (fullName ==="" || phoneNumber ==="" || Address ==="" || cardNumber ==="" || month ==="" || day ==="" || year ==="") {
+        orderDone.innerHTML="Empty Field(s). Please Try Again!";
+    }
+
+    else if(phoneNumber.length !== 8){
+        orderDone.innerHTML="Phone Number Should have 8 Digits. Please Try Again!";
+    }
+
+    else if(cardNumber.length !== 16){
+        orderDone.innerHTML="Card Number Should have 16 Digits. Please Try Again!";
+    }   
+
+    else if(phoneNumber.length !== 8){
+        orderDone.innerHTML="Phone Number Should have 8 Digits. Please Try Again!";
+    }
+
+    else if(month.length !== 2 || day.length !== 2 || year.length !== 4){
+        orderDone.innerHTML="Date is Wrong. Please Fix the Date!";
+    }
+
+    else if (!visaClicked || !masterCardClicked) {
+        orderDone.innerHTML="Please Choose Card to Buy!";
+    }
+
+    else{
+        orderDone.innerHTML="Ordered! Thanks For Shopping from Us!";
+    }
 });
